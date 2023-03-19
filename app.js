15ea8632f0250825105b4f5b46eb86b1e66494a3
Anton-Vlad <anton.vlad_viorel@yahoo.com>
2023-03-19 18:07:39 +0200

    added firebase

---
+++ app.js
@@ -0,0 +1,13 @@
+function randomFromArray(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+function getKeyString(x, y) {
+    return `${x}x${y}`;
+}
+
+(function () {
+
+    
+
+
+})();
